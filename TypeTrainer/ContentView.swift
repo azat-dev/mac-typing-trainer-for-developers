7dc449446726c1228014532a8b8d974e700ae2ea
Azat <kaumov.a.r@gmail.com>
2020-04-26 12:12:48 +0300

    Initial Commit

---
+++ TypeTrainer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TypeTrainer
+//
+//  Created by Azat Kaiumov on 26.04.2020.
+//  Copyright © 2020 Azat Kaiumov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TypeTrainerTests/TypeTrainerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TypeTrainerTests.swift
+//  TypeTrainerTests
+//
+//  Created by Azat Kaiumov on 26.04.2020.
+//  Copyright © 2020 Azat Kaiumov. All rights reserved.
+//
+
+import XCTest
+@testable import TypeTrainer
+
+class TypeTrainerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
